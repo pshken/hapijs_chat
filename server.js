b3013c4b46592dd4e34359c2107ddfd855963a52
ShaoHong <shao_hong_peh@apple.com>
2016-02-14 01:08:32 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+var Hapi = require('hapi');
+
+// Make an event emitter for managing communication
+// between hapi and socket.io code
+
+var EventEmitter = require('events');
+var notifier = new EventEmitter();
+
+// Setup API + WS server with hapi
+
+var server = new Hapi.Server();
+server.register(require('inert'), function () {});
+
+server.connection({ port: 4000, labels: ['api'] });
+server.connection({ port: 4001, labels: ['ws'] });
+
+var apiServer = server.select('api');
+var wsServer = server.select('ws');
+
+apiServer.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+
+        reply.file('./app/index.html');
+    }
+});
+
+apiServer.route({
+    method: 'POST',
+    path: '/action',
+    handler: function (request, reply) {
+        console.log(request.payload);
+        notifier.emit('action', { chatMsg: request.payload['chatMsg'] });
+        reply('Sent!');
+    }
+});
+
+// Setup websocket stuff
+var io = require('socket.io')(wsServer.listener);
+
+io.on('connection', function (socket) {
+
+    // Subscribe this socket to `action` events
+
+    notifier.on('action', function (action) {
+        socket.emit('action', action);
+    });
+});
+
+server.start(function () {
+    console.log('Server started');
+});
