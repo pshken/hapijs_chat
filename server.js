819886bed4f31e4e55513edbbf38678e7992ec89
ShaoHong <shao_hong_peh@apple.com>
2016-02-15 14:49:53 +0800

    Added support multi rooms

--- server.js
+++ server.js
@@ -33,22 +33,51 @@ apiServer.route({
         var tmpMsg = request.payload['userName'] + ": " + request.payload['chatMsg']
         notifier.emit('action', { chatMsg: tmpMsg });
         console.log(tmpMsg);
-        //reply('Sent!');
     }
 });
+
+var usernames = {};
+var rooms = ['room1','room2','room3'];
 
 // Setup websocket stuff
 var io = require('socket.io')(wsServer.listener);
 
 io.on('connection', function (socket) {
 
-    // Subscribe this socket to `action` events
-
+    //Subscribe this socket to `action` events
     notifier.on('action', function (action) {
-        socket.emit('action', action);
+        // socket.emit('action', action);
+        console.log(action);
+        io.sockets.in(socket.room).emit('updatechat', socket.username, action);
     });
+
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+    });
+
+    socket.on('adduser', function(username){
+        socket.username = username;
+        socket.room = 'room1';
+        usernames[username] = username;
+        socket.join('room1');
+        socket.emit('updatechat', 'SERVER', 'You have connected to room1');
+        socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, 'room1');
+    });
+
+    socket.on('switchRoom', function(newRoom) {
+        socket.leave(socket.room);
+        socket.join(newRoom);
+        socket.emit('updatechat', 'SERVER', 'You have connected to ' + newRoom);
+        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+        socket.room = newRoom;
+        socket.broadcast.to(newRoom).emit('updatechat', 'SERVER', socket.username + ' has joined this room.');
+        socket.emit('updaterooms', rooms, newRoom);
+    });
+
 });
 
 server.start(function () {
-    console.log('Server started');
+    console.log('Server started!');
 });
